09fb7f5b76a71a5fc2680e695be135e171fa60b7
durga1808 <durgalskshmi3713@gmail.com>
2023-08-07 14:21:18 +0530

    first commit

---
+++ src/main/java/com/strat/Result.java
@@ -0,0 +1,13 @@
+package com.strat;
+
+import com.defin.First;
+import com.defin.Second;
+
+public class Result extends First
+{
+public void Methods(){
+    First nn = new First();
+  nn.methodA();
+}
+
+}
