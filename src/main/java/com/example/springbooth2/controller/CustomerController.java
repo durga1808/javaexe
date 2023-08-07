21fac2b8c1997e62433c1b791bb0de6dd422312f
umamuthu94 <umamuthu1994@gmail.com>
2023-08-07 15:41:29 +0530

    first commit

--- src/main/java/com/example/springbooth2/controller/CustomerController.java
+++ src/main/java/com/example/springbooth2/controller/CustomerController.java
@@ -3,9 +3,13 @@ package com.example.springbooth2.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbooth2.model.Customer;
@@ -26,4 +30,42 @@ public class CustomerController {
     public List<Customer> getAll(){
         return repo.findAll();
     }
+
+    
+     @GetMapping("/findCustomer/{dept}")
+   public List<Customer> getCustomerByDept(@RequestParam String dept) {
+      return repo.findByDept(dept);
+   }
+
+    // @PutMapping("/updateCustomer/{id}")
+    // public String updateCustomer(@PathVariable Long id, @RequestBody Customer updatedCustomer) {
+    //     // Check if the customer with the given ID exists
+    //     Customer existingCustomer = repo.findById(id).orElse(null);
+    //     if (existingCustomer == null) {
+    //         return "Customer not found";
+    //     }
+
+    //     // Update the existing customer with the new details
+    //     existingCustomer.setName(updatedCustomer.getName());
+    //     existingCustomer.setDept(updatedCustomer.getDept());
+
+    //     // Save the updated customer
+    //     repo.save(existingCustomer);
+
+    //     return "Customer updated successfully";
+    // }
+
+    // @DeleteMapping("/deleteCustomer/{id}")
+    // public String deleteCustomer(@PathVariable Long id) {
+    //     // Check if the customer with the given ID exists
+    //     Customer existingCustomer = repo.findById(id).orElse(null);
+    //     if (existingCustomer == null) {
+    //         return "Customer not found";
+    //     }
+
+    //     // Delete the customer
+    //     repo.delete(existingCustomer);
+
+    //     return "Customer deleted successfully";
+    // }
 }
--- src/main/java/com/example/springbooth2/repo/CustomerRepo.java
+++ src/main/java/com/example/springbooth2/repo/CustomerRepo.java
@@ -1,9 +1,16 @@
 package com.example.springbooth2.repo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springbooth2.model.Customer;
 
 public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+
+    List<Customer> findByDept(String dept);
+
+    Optional<Customer> findById(Long id);
     
 }
