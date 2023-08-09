d8ad615c472ba808f4298706192c03ac7420e951
durga1808 <durgalskshmi3713@gmail.com>
2023-08-09 14:21:59 +0530

    added the program

---
+++ src/main/java/com/example/App1.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import com.example.java.Ex_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        Ex_1 ex = new Ex_1();
+        ex.method(1, "Durga");
+        
+    
+       
+    }
+}
---
+++ src/main/java/com/example/java/Ex_1.java
@@ -0,0 +1,9 @@
+package com.example.java;
+
+public class Ex_1 {
+   
+    public void method(int id, String name) {
+       System.out.println(id +" "+ name);
+    }
+    }
+
